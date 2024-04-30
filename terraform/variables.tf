c99c96f9f4f5738a62bc781b0f192b0cee87664e
ayushman-7 <21051553@kiit.ac.in>
2024-04-30 19:20:51 +0530

    FINAL

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -7,7 +7,8 @@ variable "region" {
 variable "clusterName" {
   description = "Name of the EKS cluster"
   type        = string
-  default     = "gitops-eks"
+  default     = "gitops-eks" 
+  
 }
 
 
